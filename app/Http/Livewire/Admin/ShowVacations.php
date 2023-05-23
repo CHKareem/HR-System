eff5fe88e14fdf5d1af3c19ff7721895d3ff915f
ASUS <chehab.kareem@gmail.com>
2023-05-23 22:27:26 +0300

    modify vacation report

--- app/Http/Livewire/Admin/ShowVacations.php
+++ app/Http/Livewire/Admin/ShowVacations.php
@@ -3,11 +3,55 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Vacation;
+use App\Models\Vacationtype;
+use App\Models\EmployeesVacation;
+use Livewire\WithPagination;
 
 class ShowVacations extends Component
 {
+
+                // Pagination
+                use WithPagination;
+                public $EmployeesVacations = null;
+                public $firstDate = Null;
+                public $secondDate = Null;
+                public $VacationName;
+                public $VacationType;
+                protected $paginationTheme = 'bootstrap';
+                protected $listeners = ['empID'];
+
+
     public function render()
     {
-        return view('livewire.admin.show-vacations');
+        $vacationNames = Vacation::all();
+        $vacationTypes = Vacationtype::all();
+
+        if($this->VacationName != Null && $this->VacationType == Null){
+            $employeesVacations =  EmployeesVacation::where([['employee_id', $this->EmployeesVacations], ['vacation_id', $this->VacationName]])->
+            whereBetween('vacationDate' ,[$this->firstDate,$this->secondDate])->paginate(5);
+        }elseif($this->VacationType != Null && $this->VacationName == Null){
+            $employeesVacations = EmployeesVacation::where([['employee_id', $this->EmployeesVacations], ['type_id', $this->VacationType]])->
+            whereBetween('vacationDate' ,[$this->firstDate,$this->secondDate])->paginate(5);;
+        }elseif($this->VacationName != Null && $this->VacationType != Null){
+            $employeesVacations =  EmployeesVacation::where([['employee_id', $this->EmployeesVacations], ['vacation_id', $this->VacationName], ['type_id', $this->VacationType]])->
+            whereBetween('vacationDate' ,[$this->firstDate,$this->secondDate])->paginate(5);
+        }elseif($this->VacationName == Null && $this->VacationType == Null){
+            $employeesVacations =  EmployeesVacation::where('employee_id', $this->EmployeesVacations)->
+            whereBetween('vacationDate' ,[$this->firstDate,$this->secondDate])->paginate(5);   
+        }
+
+
+        return view('livewire.admin.show-vacations', [
+            'vacationNames' => $vacationNames,
+            'vacationTypes' => $vacationTypes,
+            'employeesVacations' => $employeesVacations,
+        ]);
     }
+
+    public function empID($empId)
+    {
+        $this->EmployeesVacations = $empId;
+    }
+
 }
--- resources/views/livewire/admin/show-vacations.blade.php
+++ resources/views/livewire/admin/show-vacations.blade.php
@@ -23,28 +23,46 @@
                 <div class="col-lg-12">
                         <div class="d-flex justify-content-between">
                             <div class="form-group col-md-6">
-                        <label for="selectedFilter">select Filter</label>
-                        <select wire:model="selectedFilter" class="custom-select rounded-0" id="selectedFilter">
+                        <label for="vacationName">select Filter</label>
+                        <select wire:model="VacationName" class="custom-select rounded-0" id="vacationName">
                             <option selected> </option>
-                            <option value="logDate"> All Attendances </option>
-                            <option value="logIn"> LogIn Not Exist </option>
-                            <option value="logOut"> LogOut Not Exist </option>
-                            <option value="logTime"> Full Absence </option>
+                            @foreach ($vacationNames as $vacationName)
+                                <option value="{{ $vacationName->id }}">{{ $vacationName->vacationName }}</option>
+                            @endforeach
                         </select>
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="selectedFilter">select Filter</label>
-                        <select wire:model="selectedFilter" class="custom-select rounded-0" id="selectedFilter">
+                        <label for="VacationType">select Filter</label>
+                        <select wire:model="VacationType" class="custom-select rounded-0" id="vacationType">
                             <option selected> </option>
-                            <option value="logDate"> All Attendances </option>
-                            <option value="logIn"> LogIn Not Exist </option>
-                            <option value="logOut"> LogOut Not Exist </option>
-                            <option value="logTime"> Full Absence </option>
+                            @foreach ($vacationTypes as $vacationType)
+                                <option value="{{ $vacationType->id }}">{{ $vacationType->vacationType }}</option>
+                            @endforeach
                         </select>
                     </div>
                         </div>
                 </div>
 </div>
 
+
+@if($employeesVacations)
+            @foreach($employeesVacations as $employeesVacation)
+            <div class="card text-center">
+            <div class="card-header">
+                <b><img class="fa-solid fa-door-open mr-2"></b>
+            <b>{{$employeesVacation->vacationDate}}</b>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title float-none">Employee Name: <b class="ml-2">{{$employeesVacation->employees->fullName}}</b></h5>
+    <h5 class="card-title float-none">Vacation Name: <b class="ml-2">{{$employeesVacation->vacations->vacationName}}</b></h5>
+    <h5 class="card-title float-none">Vacation Type: <b class="ml-2">{{$employeesVacation->vacationtypes->vacationType}}</b></h5>
+    <h5 class="card-title float-none">Duration: <b class="ml-2">{{$employeesVacation->duration}}</b></h5>
+    <h5 class="card-title float-none">Reason: <b class="ml-2">{{$employeesVacation->reason}}</b></h5>
+    <h5 class="card-title float-none">Authorized: <b class="ml-2">{{$employeesVacation->isAuthor ? 'Authorized' : 'Not Authorized' }}</b></h5>
+  </div>
+</div>
+            @endforeach
+            {{ $employeesVacations->links() }} 
+    @endif
 
 </div>
